6775f8dc024b06690f53e24794407603318e08a9
Hyago Oliveira <hyagogow@gmail.com>
2025-12-21 10:19:38 -0300

    Add PlayerName

---
+++ Scripts/_CoreSystem/PlayerName.cs
@@ -0,0 +1,13 @@
+﻿namespace MMX.Players
+{
+    /// <summary>
+    /// All players in the game.
+    /// </summary>
+    public enum PlayerName
+    {
+        None = -1,
+        MegaManX,
+        Zero,
+        Axl
+    }
+}
